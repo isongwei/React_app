31b7c3410a1256e1e2bd6c0889a58d23a3aa702d
zhangsongwei <imsongwei@163.com>
2018-07-25 14:58:53 +0800

    init

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React, {
 } from 'react';
 import logo from './img/logo.svg';
 import './css/App.css';
-import Header from './views/header.js'
+import Header from './views/common/header.jsx'
+import Footer from './views/common/footer.jsx'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
       <Header/>
+      <Footer/>
         
       </div>
 		);
